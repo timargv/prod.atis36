a814befa103771d0b9c4cd467a280cecd56de901
Timur <tima.rgv@gmail.com>
2018-03-15 19:33:47 +0300

    Update style errors

--- resources/views/admin/errors.blade.php
+++ resources/views/admin/errors.blade.php
@@ -1,6 +1,6 @@
 @if($errors->any())
-    <div class="alert alert-danger" style="margin: 15px 15px 0;" role="alert">
-        <ul>
+    <div class="alert alert-danger" style="margin: 15px 0px 0;" role="alert">
+        <ul style="padding: 0 0 0 15px;">
             @foreach($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
