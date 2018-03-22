cab080b8e4216a6ed86cd628344b39537d3260f1
timargv <tima.rgv@mail.ru>
2018-03-22 17:01:13 +0300

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,4 +1,5 @@
 <?php
+use Carbon\Carbon;
 
 use Illuminate\Database\Seeder;
 
@@ -11,11 +12,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(MproviderTableSeeder::class);
-        $this->call(ProductTableSeeder::class);
+        // $this->call([
+        //   MproviderTableSeeder::class,
+        //   ProductTableSeeder::class,
+        //   UserTableSeeder::class,
+        //   ProviderTableSeeder::class,
+        // ]);
 
-        $this->call(UserTableSeeder::class);
-        $this->call(ProviderTableSeeder::class);
+        // DB::table('users')->insert([
+        //     [
+        //       'name' => str_random(10),
+        //       'email' => str_random(10).'@gmail.com',
+        //       'password' => bcrypt('secret'),
+        //
+        //     ],
+        //     [
+        //       'name' => str_random(10),
+        //       'email' => str_random(10).'@gmail.com',
+        //       'password' => bcrypt('secret'),
+        //
+        //     ],
+        //     [
+        //       'name' => str_random(10),
+        //       'email' => str_random(10).'@gmail.com',
+        //       'password' => bcrypt('secret'),
+        //
+        //     ]
+        //
+        // ]);
+
+
+        DB::table('products')->insert([
+          'title' => 'asdasdsa',
+          'num' => '5456',
+          'site' => 'http://atis36.ru',
+          'prc' => '1233',
+          'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          'image' => '',
+          'category_id' => '0',
+          'provider_id' => '0',
+          'status' => 1,
+          'slug' => 'sadasd',
+          'date' => Carbon::create('2000', '01', '01')
+        ]);
 
     }
 }
